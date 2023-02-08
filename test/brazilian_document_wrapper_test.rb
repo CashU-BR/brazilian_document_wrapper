d29a5866c8848ce6d3630d9cf2cd2132abf92fdd
Joao Torquato <joao.otl@gmail.com>
2023-02-08 19:40:17 -0300

    Extract method generate cnpj/cpf

--- test/brazilian_document_wrapper_test.rb
+++ test/brazilian_document_wrapper_test.rb
@@ -6,4 +6,20 @@ class BrazilianDocumentWrapperTest < ActiveSupport::TestCase
   test 'it has a version number' do
     assert BrazilianDocumentWrapper::VERSION
   end
+
+  def test_cnpj_generate_method_formatted
+    assert_match(%r{^\d{2}\.\d{3}\.\d{3}/\d{4}\-\d{2}$}, BrazilianDocumentWrapper.generate_cnpj)
+  end
+
+  def test_cnpj_generate_method_stripped
+    assert_match(/^\d{14}$/, BrazilianDocumentWrapper.generate_cnpj(false))
+  end
+
+  def test_cpf_generate_method_formatted
+    assert_match(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, BrazilianDocumentWrapper.generate_cpf)
+  end
+
+  def test_cpf_generate_method_stripped
+    assert_match(/^\d{11}$/, BrazilianDocumentWrapper.generate_cpf(false))
+  end
 end
